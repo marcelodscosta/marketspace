5e60a6301e112decb123e3ca68d5069e0bbb5304
marcelodscosta <81534727+marcelodscosta@users.noreply.github.com>
2023-06-05 09:28:03 -0300

    refactor

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -7,9 +7,9 @@ import { InputIcon } from '@components/InputIcon';
 
 export const SignIn = () => {
   return (
-    <VStack flex={1} bg="gray.600" alignItems="center">
-      <ScrollView w="full">
-        <VStack mt={24} alignItems="center" px={8}>
+    <VStack flex={1} bg="gray.700" alignItems="center">
+      <ScrollView w="full" showsVerticalScrollIndicator={false}>
+        <VStack pt={24} alignItems="center" px={8} bg="gray.600" pb={6}>
           <Logo />
           <Heading fontFamily="heading">marketspace</Heading>
           <Text fontFamily="body" fontSize="sm">
@@ -20,22 +20,20 @@ export const SignIn = () => {
             Acesse sua conta
           </Text>
 
-          <Input placeholder="E-mail" />
+          <Input placeholder="E-mail" keyboardType="email-address" />
 
           <InputIcon placeholder="Senha" />
           <Button title="Entrar" />
         </VStack>
 
-        <VStack
-          alignItems="center"
-          w="full"
-          bg="gray.700"
-          px={8}
-          mt={8}
-          pb={16}
-        >
+        <VStack alignItems="center" w="full" px={8} mt={8} pt={4}>
           <Text mt={6}>Ainda não tem acesso?</Text>
-          <Button title="Criar uma conta" variantColorFont bg="gray.500" />
+          <Button
+            _pressed={{ bg: 'gray.400' }}
+            title="Criar uma conta"
+            variantColorFont
+            bg="gray.500"
+          />
         </VStack>
       </ScrollView>
     </VStack>
