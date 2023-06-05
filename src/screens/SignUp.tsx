fc664ea173f52f8089e2a094b4b68b3e2659dea9
marcelodscosta <81534727+marcelodscosta@users.noreply.github.com>
2023-06-05 15:37:14 -0300

    Finalização das validações

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,7 +1,6 @@
 import { Center, HStack, Heading, ScrollView, Text, VStack } from 'native-base';
 
 import LogoP from '@assets/logo_p.svg';
-
 import Pencil from '@assets/pencil-simple-line.svg';
 
 import { Button } from '@components/Button';
@@ -9,7 +8,53 @@ import { Input } from '@components/Input';
 import { InputIcon } from '@components/InputIcon';
 import { UserPhoto } from '@components/UserPhoto';
 
+import { Controller, useForm } from 'react-hook-form';
+
+import { yupResolver } from '@hookform/resolvers/yup';
+
+import * as yup from 'yup';
+
+const userSignUpSchema = yup.object({
+  name: yup
+    .string()
+    .required('Informe o nome')
+    .min(4, 'Nome com no mínimo 4 caracters'),
+  email: yup
+    .string()
+    .required('Informe o e-mail')
+    .email('Informe um e-mail válido'),
+  phone: yup.string().required('Informe o telefone'),
+  password: yup
+    .string()
+    .required('Informe a senha')
+    .min(6, 'A senha tem de possuir no mínimo 6 carecters'),
+  password_confirmation: yup
+    .string()
+    .required('Confirme a senha')
+    .oneOf([yup.ref('password')], 'A confirmação da senha não confere'),
+});
+
+type DataFormProps = {
+  name: string;
+  email: string;
+  phone: string;
+  password: string;
+  password_confirmation: string;
+};
+
 export const SignUp = () => {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<DataFormProps>({
+    resolver: yupResolver(userSignUpSchema),
+  });
+
+  const handleSignUp = (data: DataFormProps) => {
+    console.log(data);
+  };
+
   return (
     <VStack flex={1} bg="gray.600" mb={1} pb={2}>
       <ScrollView showsVerticalScrollIndicator={false} pb={20}>
@@ -31,11 +76,72 @@ export const SignUp = () => {
               <Pencil />
             </Center>
           </HStack>
-          <Input placeholder="Nome" marginTop={6} />
-          <Input placeholder="E-mail" />
-          <Input placeholder="Telefone" />
-          <InputIcon placeholder="Senha" />
-          <InputIcon placeholder="Confirmar senha" />
+
+          <Controller
+            control={control}
+            name="name"
+            render={({ field: { onChange, value } }) => (
+              <Input
+                placeholder="Nome"
+                marginTop={6}
+                onChangeText={onChange}
+                value={value}
+                errorMessage={errors.name?.message}
+              />
+            )}
+          />
+
+          <Controller
+            control={control}
+            name="email"
+            render={({ field: { onChange, value } }) => (
+              <Input
+                placeholder="E-mail"
+                onChangeText={onChange}
+                value={value}
+                errorMessage={errors.email?.message}
+              />
+            )}
+          />
+
+          <Controller
+            control={control}
+            name="phone"
+            render={({ field: { onChange, value } }) => (
+              <Input
+                placeholder="Telefone"
+                onChangeText={onChange}
+                value={value}
+                errorMessage={errors.phone?.message}
+              />
+            )}
+          />
+
+          <Controller
+            control={control}
+            name="password"
+            render={({ field: { onChange, value } }) => (
+              <InputIcon
+                placeholder="Senha"
+                onChangeText={onChange}
+                value={value}
+                errorMessage={errors.password?.message}
+              />
+            )}
+          />
+
+          <Controller
+            control={control}
+            name="password_confirmation"
+            render={({ field: { onChange, value } }) => (
+              <InputIcon
+                placeholder="Confirmar senha"
+                onChangeText={onChange}
+                value={value}
+                errorMessage={errors.password_confirmation?.message}
+              />
+            )}
+          />
 
           <Button
             bg="gray.100"
@@ -45,6 +151,7 @@ export const SignUp = () => {
             _pressed={{
               bg: 'gray.200',
             }}
+            onPress={handleSubmit(handleSignUp)}
           />
 
           <Text mt={2}>Já tem uma conta?</Text>
