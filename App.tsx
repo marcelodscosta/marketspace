9994acaf8d9f76987db66d9a10bb4041fc8e51af
marcelodscosta <81534727+marcelodscosta@users.noreply.github.com>
2023-06-04 21:59:58 -0300

    Limpeza no App.tsx

--- App.tsx
+++ App.tsx
@@ -1,29 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, VStack } from 'native-base';
 
-import { Karla_400Regular, Karla_700Bold, useFonts } from '@expo-google-fonts/karla';
+import {
+  Karla_400Regular,
+  Karla_700Bold,
+  useFonts,
+} from '@expo-google-fonts/karla';
 
 export default function App() {
-
   let [fontsLoaded] = useFonts({
     Karla_400Regular,
     Karla_700Bold,
   });
+  return <VStack><Text>Página Carregada</Text></VStack>
 
-
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
